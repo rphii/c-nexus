4cf6c46834b27332ed660bd631f253a04fe407e9
rphii <raphael.seitz18@gmail.com>
2024-02-07 17:06:26 +0100

    make use of internal sub_sel and also add NODE_LEAF preprocessor

--- src/node.h
+++ src/node.h
@@ -19,11 +19,15 @@ typedef struct Node {
     //Node *rejoin;
 } Node;
 
+#define NODE_FMT_LEN_SUB    F("%zu", FG_BK_B)
+
+#define NODE_LEAF  0
+
 void node_zero(Node *node);
 void node_free(Node *node);
 
 #define ERR_NODE_PRINT "failed printing node"
-ErrDecl node_print(Node *node, bool show_desc, size_t sub_sel);
+ErrDecl node_print(Node *node, bool show_desc);
 
 #define ERR_NODE_CREATE "failed creating node"
 ErrDecl node_create(Node *node, const char *title, const char *desc, Icon icon);
@@ -31,9 +35,9 @@ ErrDecl node_create(Node *node, const char *title, const char *desc, Icon icon);
 #define ERR_NODE_FOLLOW "failed following node"
 ErrDecl node_follow(Node **node, size_t *sub_sel);
 
-void node_set(Node *node, size_t *sub_sel);
-
 void node_set_sub(Node *node, size_t *sub_sel, size_t to_set);
+
+void node_mv_vertical(Node *node, int count);
 
 #define NODE_H
 #endif
