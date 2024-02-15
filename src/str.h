66b4349a245d78796273f81a457b0988aee14897
rphii <raphael.seitz18@gmail.com>
2024-02-15 22:41:47 +0100

    introduce some handy macros

--- src/str.h
+++ src/str.h
@@ -20,9 +20,11 @@ VEC_INCLUDE(Str, str, char, BY_VAL);
 #undef VEC_SETTINGS_KEEP_ZERO_END
 #undef VEC_SETTINGS_DEFAULT_SIZE
 
-#define STR(string)     (Str){.s = string, .last = sizeof(string)/sizeof(*string)-1}
-#define STR_L(string)   (Str){.s = string, .last = strlen(string)}
-#define STR_F(s)        (int)str_length(s), str_iter_begin(s)
+#define STR(string)             (Str){.s = string, .last = sizeof(string)/sizeof(*string)-1}
+#define STR_L(string)           (Str){.s = string, .last = string ? strlen(string) : 0}
+#define STR_LL(string, length)  (Str){.s = string, .last = length}
+
+#define STR_F(s)                (int)str_length(s), str_iter_begin(s)
 
 #define ERR_STR_CAT_BACK    "failed appending string to other string"
 #define ERR_STR_FMT         "failed string formatting"
